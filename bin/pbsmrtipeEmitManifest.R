02cd3773d5c22a33f25f3861f41edafbaf8aa999
mpkocher <michael.kocher@me.com>
2015-07-05 17:40:37 -0700

    Add correct license.

---
+++ bin/pbsmrtipeEmitManifest.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+
+# Write Static MetaTasks Manifests to json
+#library("pbsmrtpipeR")
+args <- commandArgs(trailingOnly = TRUE)
+
+main <- function() {
+    cat("Starting main\n")
+    cat(args)
+    cat("Exiting main\n")
+}
+
+main()
