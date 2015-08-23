a3004f8f512c7d6ced552ad89ea6354023c1d837
mpkocher <michael.kocher@me.com>
2015-08-22 22:16:14 -0700

    Updates to tidy

--- tidy.R
+++ tidy.R
@@ -1,11 +1,11 @@
-#script to clean up code
+# script to clean up code
 library(formatR)
 
 # MK.  lifted this from https://github.com/jeroenooms/jsonlite/blob/master/tidy.R
 
-#see ?tidy.source
-options(reindent.spaces=2)
-options(replace.assign=TRUE)
-lapply(list.files("R", full.name=TRUE), function(x){
-  try(tidy.source(x, file=x, width.cutoff=80))
-})
+# see ?tidy.source
+options(reindent.spaces = 2)
+options(replace.assign = TRUE)
+lapply(list.files("R", full.name = TRUE), function(x) {
+    try(tidy.source(x, file = x, width.cutoff = 80))
+}) 
