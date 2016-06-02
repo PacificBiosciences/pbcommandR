d7004cfac8b7fdc4a748b7509099d0f59c258685
mpkocher <michael.kocher@me.com>
2016-06-01 18:30:39 -0700

    Update tests. Bump version to 0.3.0

--- tests/testthat/test.loadToolContract.R
+++ tests/testthat/test.loadToolContract.R
@@ -4,14 +4,16 @@ context("Test for loading a Tool Contract")
 path <- "dev_example_dev_txt_app_tool_contract.json"
 
 
-test_that("Simple example", {
+test_that("Sanity Example to load TC from file", {
   tc <- loadToolContractFromPath(path)
-  print(tc)
+  #print(tc)
   expect_that(10, equals(10))
 })
 
-test_that("Convert TC to json", {
+test_that("Convert TC to JSON, then load TC from JSON", {
   tc <- loadToolContractFromPath(path)
+  logging::loginfo("successfully loaded TC")
   writeToolContract(tc, "convert-tc.json")
+  logging::loginfo("completed writing tool contract")
   expect_that(10, equals(10))
 })
