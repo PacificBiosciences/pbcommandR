05dbb186118a4e9508da6c1e65217565348ddcc1
mpkocher <michael.kocher@me.com>
2016-06-05 00:50:33 -0700

    Update report serialization to support PlotGroups

--- R/pbreportsModel.R
+++ R/pbreportsModel.R
@@ -10,6 +10,7 @@ setClass("ReportTable", representation(id = "character"))
 # this should be converted from a dataframe
 setClass("ReportTableGroup", representation(id = "character", tables = "list"))
 
+# The image should be relative path to the report.json file
 setClass("ReportPlot", representation(id = "character", image = "character"))
 
 setClass("ReportPlotGroup", representation(id = "character", plots = "list"))
@@ -18,74 +19,55 @@ setClass("ReportPlotGroup", representation(id = "character", plots = "list"))
 setClass("ReportAttribute", representation(value = "numeric", id = "character", name = "character"))
 
 # Need to validate "id" format.
-setClass("Report", representation(id = "character", attributes = "list", plotGroups = "list",
+setClass("Report", representation(id = "character", version = "character", uuid = "character", attributes = "list", plotGroups = "list",
   tables = "list"))
 
 # FIXME. Add loadReportFrom
 #' @export
 writeReport <- function(r, outputPath) {
+
   # Temp hack to get this json to look correct
+  toI <- function(i) {
+    return(paste(r@id, i, sep="."))
+  }
+
   attributeToD <- function(a) {
-    return(list(id = paste(r@id, a@id, sep="."), value = a@value, name = a@name))
+    return(list(id = toI(a@id), value = a@value, name = a@name))
+  }
+
+  plotToD <- function(p) {
+    return(list(caption = NA, image = p@image, id = toI(p@id)))
+  }
+
+  plotGroupToD <- function(p) {
+    plots <- Map(plotToD, p@plots)
+    if(length(p@plots) == 0) {
+      thumbnail <- p@plots[[1]]@image
+    } else {
+      thumbnail <- NA
+    }
+
+    return(list(
+      id = toI(p@id),
+      legend = NA,
+      title = "Title",
+      thumbnail = thumbnail,
+      plots = plots))
   }
 
   attributes <- Map(attributeToD, r@attributes)
-  rx <- list(id = r@id, attibutes = attributes, dataset_uuid = list(), plotGroups = list(), tables = list())
+  plotGroups <- Map(plotGroupToD, r@plotGroups)
+
+  rx <- list(
+    id = r@id,
+    uuid = r@uuid,
+    version = r@version,
+    attibutes = attributes,
+    dataset_uuid = list(),
+    plotGroups = plotGroups,
+    tables = list())
 
   sx <- jsonlite::toJSON(rx, pretty = TRUE, auto_unbox = TRUE)
   write(sx, file = outputPath)
   return(sx)
-}
-
-
-getMockAttributeGroup <- function() {
-  # In the python code the ids are automatically turned into absolute ids via the
-  # container Not sure this is a great idea
-  a1 <- methods::new("ReportAttribute", id = "n50", value = 1234, name = "n50")
-  a2 <- methods::new("ReportAttribute", id = "mean_readlength", value = 4567, name = "Mean Readlength")
-  ag <- list(a1, a2)
-  attributeGroup <- methods::new("ReportAttributeGroup", id = "pbmilhouse.subread", attributes = ag)
-  return(attributeGroup)
-}
-
-getMockPlotGroup <- function() {
-  plotGroupId <- "pbmilhouse.readlength_plot"
-
-  # see the above comment regarding ids
-  p1 <- methods::new("ReportPlot", id = "readlength", image = "/path/to/image.png")
-  p2 <- methods::new("ReportPlot", id = "accuracy", image = "/path/to/image2.png")
-  pg <- methods::new("ReportPlotGroup", id = plotGroupId, plots = list(p1, p2))
-  return(pg)
-}
-
-getMockTableGroup <- function() {
-  # this should just converted from a dataframe
-  t <- methods::new("ReportTable", id = "my_table")
-  t2 <- methods::new("ReportTable", id = "my_readlength_table")
-  tables <- list(t, t2)
-  tg <- methods::new("ReportTableGroup", id = "mytable_id", tables = tables)
-  return(tg)
-}
-
-getMockReport <- function() {
-  plotGroup <- getMockPlotGroup()
-  attributeGroup <- getMockAttributeGroup()
-  tableGroup <- getMockTableGroup()
-  report <- methods::new("Report", id = "pbmilhouse_report_example", plotGroups = list(plotGroup),
-    attributeGroups = list(attributeGroup), tableGroups = list(tableGroup))
-
-  return(report)
-}
-
-testBasicReport <- function() {
-  r <- getMockReport()
-
-  if (r@id != "pbmilhouse_report_example") {
-    stop("Malformed id")
-  }
-
-  s <- jsonlite::toJSON(r)
-
-  cat("Json Report\n")
-  cat(s)
 }
--- tests/testthat/test.pbreportsModel.R
+++ tests/testthat/test.pbreportsModel.R
@@ -1,19 +1,65 @@
 library(pbcommandR)
+library(uuid)
 library(logging)
 
 context("Test for serializing pbreports Model to JSON")
 
 test_that("Simple Report example", {
 
+
+  getMockAttributeGroup <- function() {
+    # In the python code the ids are automatically turned into absolute ids via the
+    # container Not sure this is a great idea
+    a1 <- methods::new("ReportAttribute", id = "n50", value = 1234, name = "n50")
+    a2 <- methods::new("ReportAttribute", id = "mean_readlength", value = 4567, name = "Mean Readlength")
+    ag <- list(a1, a2)
+    return(ag)
+  }
+
+  getMockPlotGroup <- function() {
+    plotGroupId <- "plotgroupa.readlength_plot"
+
+    # see the above comment regarding ids
+    p1 <- methods::new("ReportPlot", id = "readlength", image = "image-relative-path.png")
+    p2 <- methods::new("ReportPlot", id = "accuracy", image = "image2-relative-path.png")
+    pg <- methods::new("ReportPlotGroup", id = plotGroupId, plots = list(p1, p2))
+    return(pg)
+  }
+
+  getMockTableGroup <- function() {
+    # this should just converted from a dataframe
+    t <- methods::new("ReportTable", id = "my_table")
+    t2 <- methods::new("ReportTable", id = "my_readlength_table")
+    tables <- list(t, t2)
+    tg <- methods::new("ReportTableGroup", id = "mytable_id", tables = tables)
+    return(tg)
+  }
+
+  getMockReport <- function() {
+    plotGroup <- getMockPlotGroup()
+    attributeGroup <- getMockAttributeGroup()
+    tableGroup <- getMockTableGroup()
+    report <- methods::new("Report", id = "pbmilhouse_report_example", plotGroups = list(plotGroup),
+      attributeGroups = list(attributeGroup), tableGroups = list(tableGroup))
+
+    return(report)
+  }
+
   reportOutputPath <- tempfile(pattern = "file", tmpdir = tempdir(), fileext = "report.json")
+  # reportOutputPath <- "example-report.json"
   logging::loginfo(paste("Report output ", reportOutputPath))
-  a1 <- methods::new("ReportAttribute", id = "a1", value = 1234, name = "Metric 1")
-  a2 <- methods::new("ReportAttribute", id = "a2", value = 45.67, name = "Metric 2")
 
+  attributes <- getMockAttributeGroup()
+  plotGroups <- list(getMockPlotGroup())
+  tables <- list(getMockPlotGroup)
 
-  report <- methods::new("Report", id = "pbcommandR_hello_reseq", plotGroups = list(),
-  attributes = c(a1, a2), tables = list())
+  reportUUID <- uuid::UUIDgenerate()
+  version <- "3.1.0"
+
+  report <- methods::new("Report", uuid = reportUUID, version = version, id = "pbcommandr_hello_reseq", plotGroups = plotGroups,
+  attributes = attributes, tables = tables)
   writeReport(report, reportOutputPath)
+  cat(paste("Writing report ", reportOutputPath))
 
   expect_that(10, equals(10))
 })
